d4ba3b68dec08078ca65c717b0a325f13d19abe7
d9magai <kumagai@sencorp.co.jp>
2017-08-30 23:05:53 +0900

    define STATIC_ROOT

--- djangocms/urls.py
+++ djangocms/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from os.path import stat
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
