9a611bbf7bdb9e44ee5843a55e476d9d9bf9fbfb
d9magai <kumagai@sencorp.co.jp>
2017-09-02 18:52:52 +0900

    modify urlpatterns

--- djangocms/urls.py
+++ djangocms/urls.py
@@ -22,4 +22,4 @@ from os.path import stat
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^cms/', include('cms.urls', namespace='cms')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
