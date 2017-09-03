1c392c663723ad4d27221364f1326c142953378c
d9magai <kumagai@sencorp.co.jp>
2017-09-03 20:22:55 +0900

    implements views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.utils.encoding import python_2_unicode_compatible
+from django.views.generic import View
+
+
+@python_2_unicode_compatible
+class Index(View):
+    @method_decorator(login_required)
+    def get(self, request):
+        return render(request, 'accounts/index.html')
--- djangocms/urls.py
+++ djangocms/urls.py
@@ -13,17 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth.views import login, logout_then_login
 from os.path import stat
-from cms.urls import router as cms_router
 from rest_framework_jwt.views import obtain_jwt_token
+
+from accounts.views import Index
+from cms.urls import router as cms_router
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^cms/', include('cms.urls', namespace='cms')),
     url(r'^api/', include(cms_router.urls)),
     url(r'^api-auth/', obtain_jwt_token),
+    url(r'^accounts/login/$', login,
+        {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^accounts/logout/$', logout_then_login, name='logout'),
+    url(r'^accounts/index/$', Index.as_view(), name='account_index'),
 ]
