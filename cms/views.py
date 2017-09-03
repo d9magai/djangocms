c0ff5abfaaa5e89f58679cec15fc2d17fabf2a68
d9magai <kumagai@sencorp.co.jp>
2017-09-03 21:09:26 +0900

    use BookList

--- cms/views.py
+++ cms/views.py
@@ -1,20 +1,27 @@
-from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.list import ListView
-from cms.models import Book, Impression
-from cms.forms import BookForm, ImpressionForm
 import django_filters
 from rest_framework import viewsets, filters, permissions
+
+from cms.forms import BookForm, ImpressionForm
+from cms.models import Book, Impression
+
 from .models import Book, Impression
 from .serializer import BookSerializer, ImpressionSerializer
 
 
-def book_list(request):
-    """書籍の一覧"""
-    books = Book.objects.all().order_by('id')
-    return render(request,
-                  'cms/book_list.html',
-                  {'books': books})
+class BookList(ListView):
+    context_object_name = 'books'
+    template_name = 'cms/book_list.html'
+    pagination = 3
+
+    def get(self, request, *args, **kwargs):
+        books = Book.objects.all().order_by('id')
+        self.object_list = books
+        context = self.get_context_data(object_list=self.object_list)
+        return self.render_to_response(context)
+
 
 def book_edit(request, book_id=None):
     """書籍の編集"""
@@ -24,7 +31,8 @@ def book_edit(request, book_id=None):
         book = Book()
 
     if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)  # POST された request データからフォームを作成
+        # POST された request データからフォームを作成
+        form = BookForm(request.POST, instance=book)
         if form.is_valid():    # フォームのバリデーション
             book = form.save(commit=False)
             book.save()
@@ -34,16 +42,18 @@ def book_edit(request, book_id=None):
 
     return render(request, 'cms/book_edit.html', dict(form=form, book_id=book_id))
 
+
 def book_del(request, book_id):
     """書籍の削除"""
     book = get_object_or_404(Book, pk=book_id)
     book.delete()
     return redirect('cms:book_list')
 
+
 class ImpressionList(ListView):
     """感想の一覧"""
-    context_object_name='impressions'
-    template_name='cms/impression_list.html'
+    context_object_name = 'impressions'
+    template_name = 'cms/impression_list.html'
     paginate_by = 2  # １ページは最大2件ずつでページングする
 
     def get(self, request, *args, **kwargs):
@@ -51,8 +61,10 @@ class ImpressionList(ListView):
         impressions = book.impressions.all().order_by('id')   # 書籍の子供の、感想を読む
         self.object_list = impressions
 
-        context = self.get_context_data(object_list=self.object_list, book=book)    
+        context = self.get_context_data(
+            object_list=self.object_list, book=book)
         return self.render_to_response(context)
+
 
 def impression_edit(request, book_id, impression_id=None):
     """感想の編集"""
@@ -63,18 +75,21 @@ def impression_edit(request, book_id, impression_id=None):
         impression = Impression()
 
     if request.method == 'POST':
-        form = ImpressionForm(request.POST, instance=impression)  # POST された request データからフォームを作成
+        # POST された request データからフォームを作成
+        form = ImpressionForm(request.POST, instance=impression)
         if form.is_valid():    # フォームのバリデーション
             impression = form.save(commit=False)
             impression.book = book  # この感想の、親の書籍をセット
             impression.save()
             return redirect('cms:impression_list', book_id=book_id)
     else:    # GET の時
-        form = ImpressionForm(instance=impression)  # impression インスタンスからフォームを作成
+        # impression インスタンスからフォームを作成
+        form = ImpressionForm(instance=impression)
 
     return render(request,
                   'cms/impression_edit.html',
                   dict(form=form, book_id=book_id, impression_id=impression_id))
+
 
 def impression_del(request, book_id, impression_id):
     """感想の削除"""
@@ -87,7 +102,8 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    
+
+
 class ImpressionViewSet(viewsets.ModelViewSet):
     queryset = Impression.objects.all()
     serializer_class = ImpressionSerializer
