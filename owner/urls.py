d6c0e2ed5829c12b6cc5134327bbef219ff3cfea
d9magai <kumagai@sencorp.co.jp>
2017-09-09 19:35:44 +0900

    login -> mypage

--- owner/urls.py
+++ owner/urls.py
@@ -1,19 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
-from owner import views 
- 
+from owner import views
+from .views import LoginView
+
 app_name = 'owner'
- 
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^mypage/$', views.mypage, name='mypage'),
     url(r'^redirect/$', views.redirect, name='redirect'),
-    url(
-        r'^login/$',
-        auth_views.login,
-        {'template_name': 'owner/login.html'},
-        name='login'
-    ),
+    url(r'^login/$', LoginView.as_view(), name='login'),
     url(
         r'^logout/$',
         auth_views.logout,
