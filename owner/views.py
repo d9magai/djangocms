400b21b89d24f174ba4170be2ea6ed8e27f6270b
d9magai <kumagai@sencorp.co.jp>
2017-09-12 07:35:39 +0900

    upload

--- owner/views.py
+++ owner/views.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import base64
 from datetime import datetime, timedelta, timezone
 from django.conf import settings
 from django.contrib.auth import authenticate, login
@@ -11,9 +12,9 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import FormView
 from django.views.generic.list import ListView
-import json
-import hmac
 import hashlib
+import hmac
+import json
 
 from .forms import LoginForm, BookForm, ImpressionForm
 from .models import Book, Impression
@@ -158,34 +159,59 @@ def impression_del(request, book_id, impression_id):
     return HttpResponseRedirect(reverse('owner:impression_list', kwargs={'book_id': book_id}))
 
 
+def sign(key, msg):
+    return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
+
+
+def getSignatureKey(key, date_stamp, regionName, serviceName):
+    kDate = sign(('AWS4' + key).encode('utf-8'), date_stamp)
+    kRegion = sign(kDate, regionName)
+    kService = sign(kRegion, serviceName)
+    kSigning = sign(kService, 'aws4_request')
+    return kSigning
+
+
 @login_required(login_url='/owner/login/')
 @csrf_exempt
 def policies(request):
     key = '%s/%s' % (request.user.id, datetime.now().strftime("%Y%m%d%H%M%S"))
     content_type = request.POST['content_type']
     size = request.POST['size']
-    policy_document = json.dump({
-        'expiration': datetime.now(timezone('UTC')) + datetime.timedelta(minute=1),
-        'conditions': {
+    t = datetime.utcnow()
+    dict = {
+        'expiration': (t + timedelta(minutes=1)).isoformat(),
+        'conditions': [
             {'bucket': settings.S3_BUCKET},
             {'key': key},
             {'Content-Type': content_type},
-            {'content-length-range', size, size}
-        }
-    })
-    policy = policy_document.encode('base64')
+            ['content-length-range', size, size]
+        ]
+    }
+    policy_document = json.dumps(dict)
+    policy = base64.b64encode(policy_document.encode('utf-8'))
 
-    # signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'),AWS_SECRET_KEY, policy)).gsub("\n", '')
+    date_stamp = t.strftime('%Y%m%d')
+    service = 's3'
+    region = 'ap-northeast-1'
+    signing_key = getSignatureKey(settings.AWS_SECRET_ACCESS_KEY, date_stamp, region, service)
+    signature = hmac.new(signing_key, policy, hashlib.sha256).hexdigest()
 
-    signature = 'sig'
-    policy = 'po'
+    algorithm = 'AWS4-HMAC-SHA256'
+    credential_scope = date_stamp + '/' + region + '/' + service + '/' + 'aws4_request'
+    signed_headers = 'content-type;host;x-amz-date;x-amz-target'
+    authorization_header = algorithm + ' ' + 'Credential=' + settings.AWS_ACCESS_KEY_ID + '/' + credential_scope + ', ' + 'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
+
+    amz_date = t.strftime('%Y%m%dT%H%M%SZ')
     array = {
         "url": "https://" + settings.S3_BUCKET + ".s3.amazonaws.com/",
         'form': {
             'AWSAccessKeyId': settings.AWS_ACCESS_KEY_ID,
             'signature': signature,
-            'policy': policy,
-            'key': key
+            'policy': policy.decode('utf-8'),
+            'key': key,
+            'Content-Type': content_type,
+            'X-Amz-Date': amz_date,
+            'Authorization': authorization_header
         }
     }
     return JsonResponse(array)
--- static/js/main.js
+++ static/js/main.js
@@ -17,6 +17,34 @@ $(function() {
         console.log(name + ":" + data.form[name]);
       }
       fd.append('file', file);
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+    switch ( xhr.readyState ) {
+        case 0:
+            // 未初期化状態.
+            console.log( 'uninitialized!' );
+            break;
+        case 1: // データ送信中.
+            console.log( 'loading...' );
+            break;
+        case 2: // 応答待ち.
+            console.log( 'loaded.' );
+            break;
+        case 3: // データ受信中.
+            console.log( 'interactive... '+xhr.responseText.length+' bytes.' );
+            break;
+        case 4: // データ受信完了.
+            if( xhr.status == 200 || xhr.status == 304 ) {
+                var data = xhr.responseText; // responseXML もあり
+                console.log( 'COMPLETE! :'+data );
+            } else {
+                console.log( 'Failed. HttpStatus: '+xhr.statusText );
+            }
+            break;
+    }
+};
+      xhr.open('POST', data.url, true)
+      xhr.send(fd);
     })
     $(this).val('');
   });
