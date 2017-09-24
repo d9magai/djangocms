8695eb9867af3132cfe30b88e3a476ecd7420aec
d9magai <kumagai@sencorp.co.jp>
2017-09-24 18:20:19 +0900

    modify main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('#inputFile').change(function (e){
+  $('#inputFile').change(function(e) {
     var file = e.target.files[0];
     console.log(file.name)
     $.ajax({
@@ -10,42 +10,43 @@ $(function() {
         size: file.size,
         content_type: file.type
       }
-    }).done(function (data) {
+    }).done(function(data) {
       var name, fd = new FormData();
-      for (name in data.form) if (data.form.hasOwnProperty(name)) {
-        fd.append(name, data.form[name]);
-        console.log(name + ":" + data.form[name]);
-      }
+      for (name in data.form)
+        if (data.form.hasOwnProperty(name)) {
+          fd.append(name, data.form[name]);
+          console.log(name + ":" + data.form[name]);
+        }
       fd.append('file', file);
       var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function() {
-    switch ( xhr.readyState ) {
+        switch (xhr.readyState) {
         case 0:
-            // 未初期化状態.
-            console.log( 'uninitialized!' );
-            break;
+          // 未初期化状態.
+          console.log('uninitialized!');
+          break;
         case 1: // データ送信中.
-            console.log( 'loading...' );
-            break;
+          console.log('loading...');
+          break;
         case 2: // 応答待ち.
-            console.log( 'loaded.' );
-            break;
+          console.log('loaded.');
+          break;
         case 3: // データ受信中.
-            console.log( 'interactive... '+xhr.responseText.length+' bytes.' );
-            break;
+          console.log('interactive... ' + xhr.responseText.length + ' bytes.');
+          break;
         case 4: // データ受信完了.
-            console.log(xhr.responseText);
-            if( xhr.status == 200 || xhr.status == 304 ) {
-                var data = xhr.responseText; // responseXML もあり
-                console.log( 'COMPLETE! :'+data );
-            } else {
-                console.log( 'Failed. HttpStatus: '+xhr.statusText );
-            }
-            break;
-    }
-};
-      xhr.onerror = function (e) {
-                console.error(xhr.statusText);
+          console.log(xhr.responseText);
+          if (xhr.status == 200 || xhr.status == 304) {
+            var data = xhr.responseText; // responseXML もあり
+            console.log('COMPLETE! :' + data);
+          } else {
+            console.log('Failed. HttpStatus: ' + xhr.statusText);
+          }
+          break;
+        }
+      };
+      xhr.onerror = function(e) {
+        console.error(xhr.statusText);
       };
       xhr.open('POST', data.url, true)
       xhr.send(fd);
@@ -53,4 +54,3 @@ $(function() {
     $(this).val('');
   });
 });
-
